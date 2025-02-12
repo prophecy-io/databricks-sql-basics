5aa76d0dccb70ad83e7adb6e4cf914fd265f57ef
abhiyad <abhishekcms611@gmail.com>
2025-02-12 09:49:33 +0530

    init with gem

---
+++ gems/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+setup(
+    name = 'DatabricksSqlBasics',
+    version = '0.0.1',
+    packages = ['AlteryxDatabricks'],
+    package_dir = {'AlteryxDatabricks': '.'},
+    description = '',
+    install_requires = [],
+)
+
