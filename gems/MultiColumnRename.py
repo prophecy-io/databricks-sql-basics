7e8625c07ee9219c6d2af83baa348aba5cced929
Debashis Adak <debashisadak@Debashiss-MacBook-Pro.local>
2025-04-07 16:39:06 +0530

    regression changes - Remove column parser and rename BulkColumnExpression

--- gems/MultiColumnRename.py
+++ gems/MultiColumnRename.py
@@ -12,7 +12,7 @@ from pyspark.sql.types import StructType, StructField, StringType, IntegerType, 
 class MultiColumnRename(MacroSpec):
     name: str = "MultiColumnRename"
     projectName: str = "DatabricksSqlBasics"
-    category: str = "Transform"
+    category: str = "Prepare"
 
 
     @dataclass(frozen=True)
