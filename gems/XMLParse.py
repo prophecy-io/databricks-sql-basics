c39ebffc24feb88b4a0d02907b451a055c54c1a5
Debashis Adak <debashisadak@Debashiss-MacBook-Pro.local>
2025-06-02 16:01:10 +0530

    1st commit - non empty params changes

--- gems/XMLParse.py
+++ gems/XMLParse.py
@@ -1,11 +1,9 @@
 from dataclasses import dataclass
-import dataclasses
-import json
 
-from collections import defaultdict
 from prophecy.cb.sql.Component import *
 from prophecy.cb.sql.MacroBuilderBase import *
 from prophecy.cb.ui.uispec import *
+
 
 class XMLParse(MacroSpec):
     name: str = "XMLParse"
@@ -175,8 +173,8 @@ class XMLParse(MacroSpec):
             "'" + sampleRecord + "'",
             "'" + sampleSchema + "'"
         ]
-        non_empty_param = ",".join([param for param in arguments if param != ''])
-        return f'{{{{ {resolved_macro_name}({non_empty_param}) }}}}'
+        params = ",".join([param for param in arguments])
+        return f'{{{{ {resolved_macro_name}({params}) }}}}'
 
     def loadProperties(self, properties: MacroProperties) -> PropertiesType:
         parametersMap = self.convertToParameterMap(properties.parameters)
