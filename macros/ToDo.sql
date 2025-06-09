9816a18dfe16564dfebe361dca9a2ab84c28a969
abhinavjain <abhinav@simpledatalabs.com>
2025-06-09 19:45:20 +0530

    to do gem initial commit

--- macros/ToDo.sql
+++ macros/ToDo.sql
@@ -1,4 +1,4 @@
-{% macro Todo(relation, schema, error_string, code_string) %}
+{% macro ToDo(relation, schema, error_string, code_string) %}
  select
      {% for column in schema %}
          CAST(NULL AS {{ column.type }}) as {{ column.name }}{% if not loop.last %},{% endif %}
