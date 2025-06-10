61e653cb1f29dce2060465524d0eedf11a62c24f
abhinavjain <abhinav@simpledatalabs.com>
2025-06-10 17:38:41 +0530

    to do gem initial commit

--- macros/ToDo.sql
+++ macros/ToDo.sql
@@ -1,7 +1,7 @@
 {% macro ToDo(diag_message) %}
     SELECT *
     FROM (
-        SELECT 1 as error_message
+        SELECT cast(1 as string) as error_message
     ) AS dummy
     WHERE raise_error('ToDo: {{ diag_message }}') IS NULL
 {% endmacro %}
