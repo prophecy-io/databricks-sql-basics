02ab3fb3290fb6f4010114aeda770660753ec65d
jainabhinav <103921419+jainabhinav@users.noreply.github.com>
2025-06-18 11:43:13 +0530

    Abhinav/transpose case insensitive (#24)
    make transpose case insensitive

--- gems/setup.py
+++ gems/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'DatabricksSqlBasics',
-    version = '0.0.8',
+    version = '0.0.9.dev0',
     packages = ['DatabricksSqlBasics'],
     package_dir = {'DatabricksSqlBasics': '.'},
     description = '',
