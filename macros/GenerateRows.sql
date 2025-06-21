62013cefa8644c1f218750443a7d1d96511bbc07
abhinavjain <abhinav@simpledatalabs.com>
2025-06-21 14:45:20 +0530

    generate rows

--- macros/GenerateRows.sql
+++ macros/GenerateRows.sql
@@ -1,59 +1,56 @@
 {% macro GenerateRows(
-        relation_name,          -- table or '' for stand-alone
-        new_field_name,         -- column to create
-        start_expr,             -- literal 2025-01-01  or column
-        end_expr,               -- literal or column
-        step_expr      = 1,     -- literal or column
-        data_type      = 'int', -- int / bigint / float / decimal / date / timestamp
-        interval_unit  = 'day'  -- for date / timestamp
+        relation_name,
+        new_field_name,
+        start_expr,
+        end_expr,
+        step_expr      = 1,
+        data_type      = 'int',
+        interval_unit  = 'day'
 ) %}
-{%- set nums = ['int','integer','bigint','float','double','decimal'] -%}
-{%- set tbl  = relation_name | trim -%}
-{%- set has_tbl = tbl != '' -%}
 
-{%- set s_tok = start_expr|string -%}
-{%- set e_tok = end_expr|string -%}
+{%- set num_types = ['int','integer','bigint','float','double','decimal'] -%}
+{%- set tbl       = relation_name | trim -%}
+{%- set has_tbl   = tbl != '' -%}
 
-{%- if data_type not in nums -%}
-    {%- if ("-" in s_tok or ":" in s_tok) and ("'" not in s_tok) and ("(" not in s_tok) -%}
-        {%- set s_tok = "'" ~ s_tok ~ "'" -%}
-    {%- endif -%}
-    {%- if ("-" in e_tok or ":" in e_tok) and ("'" not in e_tok) and ("(" not in e_tok) -%}
-        {%- set e_tok = "'" ~ e_tok ~ "'" -%}
-    {%- endif -%}
-{%- endif -%}
-
-{%- if data_type in nums -%}
-    {%- set cast_start = "CAST(" ~ s_tok ~ " AS " ~ data_type ~ ")" -%}
-    {%- set cast_end   = "CAST(" ~ e_tok ~ " AS " ~ data_type ~ ")" -%}
-    {%- set step_part  = step_expr -%}
+{%- if data_type in num_types -%}
+    {%- set cast_start = "CAST(" ~ start_expr ~ " AS " ~ data_type ~ ")" -%}
+    {%- set cast_end   = "CAST(" ~ end_expr   ~ " AS " ~ data_type ~ ")" -%}
+    {%- set step_val   = step_expr -%}
 {%- else -%}
+    {%- set s_tok = ("'" ~ start_expr ~ "'")
+                    if ("-" in start_expr or ":" in start_expr) and "'" not in start_expr
+                    else start_expr -%}
+    {%- set e_tok = ("'" ~ end_expr ~ "'")
+                    if ("-" in end_expr   or ":" in end_expr)   and "'" not in end_expr
+                    else end_expr -%}
     {%- set cast_start = "CAST(" ~ s_tok ~ " AS " ~ data_type ~ ")" -%}
     {%- set cast_end   = "CAST(" ~ e_tok ~ " AS " ~ data_type ~ ")" -%}
-    {%- set step_part  = step_expr ~ ' ' ~ interval_unit -%}
+    {%- set step_val   = step_expr ~ ' ' ~ interval_unit -%}
 {%- endif -%}
 
 (
 {%- if not has_tbl -%}
-
-    {%- if data_type in nums -%}
-        SELECT explode(sequence({{ cast_start }}, {{ cast_end }}, {{ step_part }})) AS {{ new_field_name }}
+    {%- if data_type in num_types -%}
+        SELECT explode(sequence({{ cast_start }}, {{ cast_end }}, {{ step_val }}))
+               AS {{ new_field_name }}
     {%- else -%}
-        SELECT explode(sequence({{ cast_start }}, {{ cast_end }}, interval {{ step_part }})) AS {{ new_field_name }}
+        SELECT explode(sequence({{ cast_start }}, {{ cast_end }},
+                                interval {{ step_val }}))
+               AS {{ new_field_name }}
     {%- endif -%}
-
 {%- else -%}
-
-    {%- if data_type in nums -%}
+    {%- if data_type in num_types -%}
         SELECT r.*, val AS {{ new_field_name }}
         FROM {{ tbl }} AS r
-        LATERAL VIEW explode(sequence({{ cast_start }}, {{ cast_end }}, {{ step_part }})) t AS val
+        LATERAL VIEW explode(sequence({{ cast_start }}, {{ cast_end }}, {{ step_val }}))
+                    t AS val
     {%- else -%}
         SELECT r.*, val AS {{ new_field_name }}
         FROM {{ tbl }} AS r
-        LATERAL VIEW explode(sequence({{ cast_start }}, {{ cast_end }}, interval {{ step_part }})) t AS val
+        LATERAL VIEW explode(sequence({{ cast_start }}, {{ cast_end }},
+                                      interval {{ step_val }}))
+                    t AS val
     {%- endif -%}
-
 {%- endif -%}
 )
 {% endmacro %}
