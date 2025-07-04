60a126c36b42a8e26be4e80c35be62988576a946
Ayush Gupta <ayushgupta@Ayushs-MacBook-Pro.local>
2025-07-04 19:40:56 +0530

    commiting setup.py and macros yml files for DataEncoder Gem

--- gems/setup.py
+++ gems/setup.py
@@ -2,10 +2,11 @@
 from setuptools import setup, find_packages
 setup(
     name = 'DatabricksSqlBasics',
-    version = '0.0.11.dev0',
+    version = '0.0.11.dev1',
     packages = ['DatabricksSqlBasics'],
     package_dir = {'DatabricksSqlBasics': '.'},
     description = '',
     install_requires = [],
 )
+
 
