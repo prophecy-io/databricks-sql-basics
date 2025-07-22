0ad57a307bc14291749019bedb44c9a88057a3d5
sudoAyush1 <g.ayush@prophecy.io>
2025-07-23 02:25:33 +0530

    DataMasking Gem (#32)

--- gems/setup.py
+++ gems/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'DatabricksSqlBasics',
-    version = '0.0.11',
+    version = '0.0.12.dev0',
     packages = ['DatabricksSqlBasics'],
     package_dir = {'DatabricksSqlBasics': '.'},
     description = '',
