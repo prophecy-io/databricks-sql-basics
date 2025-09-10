0493d6556caed1e52bf56fef3856acfeb473e230
abhinavjain <abhinav@simpledatalabs.com>
2025-09-10 20:01:51 +0530

    black formatting for python files

--- gems/setup.py
+++ gems/setup.py
@@ -1,11 +1,10 @@
-
 from setuptools import setup, find_packages
-setup(
-    name = 'DatabricksSqlBasics',
-    version = '0.0.13.dev0',
-    packages = ['DatabricksSqlBasics'],
-    package_dir = {'DatabricksSqlBasics': '.'},
-    description = '',
-    install_requires = [],
-)
 
+setup(
+    name="DatabricksSqlBasics",
+    version="0.0.13.dev0",
+    packages=["DatabricksSqlBasics"],
+    package_dir={"DatabricksSqlBasics": "."},
+    description="",
+    install_requires=[],
+)
